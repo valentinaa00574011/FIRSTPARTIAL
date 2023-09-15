4c96fa29a24eabcc4fcd0a9cbff4fc179b0ef79a
valentinaa00574011 <145053296+valentinaa00574011@users.noreply.github.com>
2023-09-15 09:47:46 -0600

    Create replitcodes.py
    1  function machine code
    1  Coding Rush code
    1 code that prints "Hello world"
    2 exam review exercises
    1 code from your midterm exam

---
+++ replitcodes.py
@@ -0,0 +1,23 @@
+#functionmachine
+def f(x):
+  f_x=-2*x+34
+  return f_x
+
+y=f(10)
+print(y)
+
+def f(x):
+  f_x=x/3-10
+  return f_x
+
+y=f(3)
+print(y)
+
+def f(x):
+  f_x=-abs(3*x)
+  return f_x
+
+y= f(1)
+print(y)
+
+#
